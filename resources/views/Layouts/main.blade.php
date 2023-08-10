89937f57e74cb5443881bc860e84ed93507f7781
Ghayth-Moustpha <ghayth.moustpha@gmail.com>
2023-08-10 12:04:40 +0300

    Develped the Main Layout

---
+++ resources/views/Layouts/main.blade.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html class="no-js" lang="en"> 
+
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="NSHAA is a Group of private companies providing Construction Services for Power Plants, Refineries and oil plants, Gas & Oil surface facilities and gathering stations, Cement Plants, Wheat Mills, Water and Water Treatment Plants, and various industrial plants. To achieve success in the work, INSHAA Group depends on the cumulative experience formed through executed projects over the decades, its professional staff who have a long experience in various types of work, and the necessary equipment for projects' implementation">
+<meta name="author" content="Ghayth Moutapha | Software Engineer">
+<title> 
+   {{$pageTitle}}   
+</title>
+<link rel="shortcut icon" type="image/x-icon" href="img/logo-dark.png">
+
+<link rel="stylesheet" href="css/fontawesome.min.css">
+
+<link rel="stylesheet" href="css/themify-icons.css">
+
+<link rel="stylesheet" href="css/elegant-line-icons.css">
+
+<link rel="stylesheet" href="css/elegant-font-icons.css">
+
+<link rel="stylesheet" href="css/flaticon.css">
+
+<link rel="stylesheet" href="css/animate.min.css">
+
+<link rel="stylesheet" href="./css/bootstrap.min.css">
+
+<link rel="stylesheet" href="css/slick.css">
+
+<link rel="stylesheet" href="css/slider.css">
+
+<link rel="stylesheet" href="css/odometer.min.css">
+
+<link rel="stylesheet" href="css/venobox/venobox.css">
+
+<link rel="stylesheet" href="css/owl.carousel.css">
+
+<link rel="stylesheet" href="css/main.css">
+
+<link rel="stylesheet" href="css/responsive.css">
+<script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+</head>
+<body> 
+    
+@yield('content') 
+
+<footer class="footer-section align-center">
+<div class="container">
+<h4><a href="https://eliejah.com" class="text-white">Powered By: Ghayth Moustpha, Software Engineer </a></h4>
+</div>
+</footer>
+<a data-scroll href="#header" id="scroll-to-top"><i class="arrow_carrot-up"></i></a>
+
+<script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="js/vendor/jquery-1.12.4.min.js"></script>
+
+<script src="js/vendor/bootstrap.min.js"></script>
+
+<script src="js/vendor/tether.min.js"></script>
+
+<script src="js/vendor/headroom.min.js"></script>
+
+<script src="js/vendor/owl.carousel.min.js"></script>
+
+<script src="js/vendor/smooth-scroll.min.js"></script>
+
+<script src="js/vendor/venobox.min.js"></script>
+
+<script src="js/vendor/jquery.ajaxchimp.min.js"></script>
+
+<script src="js/vendor/slick.min.js"></script>
+
+<script src="js/vendor/waypoints.min.js"></script>
+
+<script src="js/vendor/odometer.min.js"></script>
+
+<script src="js/vendor/wow.min.js"></script>
+
+<script src="js/main.js"></script>
